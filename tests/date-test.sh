45ea1333add6ac087848799273ace7fd9b06dd5d
artem77chernyshev <135270513+artem77chernyshev@users.noreply.github.com>
2023-07-05 13:03:56 +0300

    Update date-test.sh

--- tests/date-test.sh
+++ tests/date-test.sh
@@ -7,6 +7,7 @@ output=$(./app)
 expected_output="05.07.2023"
 
 if [ "$output" == "$expected_output" ]; then
-echo "Test succesfull: $output"
+echo "Success: $output"
 else
-echo "Test failed. Expected output: $expected_output"
+echo "Fail: $expected_output, >
+fi
