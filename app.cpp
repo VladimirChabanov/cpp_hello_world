1c550951c2a0b0fccd2275f5874b8818d37cd2d4
Anastasia2804m <136562209+Anastasia2804m@users.noreply.github.com>
2023-07-05 13:37:58 +0300

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
+#include <string>
  
-int main(std::cout << "05.07.2023" << std::endl;){
-    std::cout << "Hello World" << std::endl;
+int main(){
+    std::cout << "05.07.2023" << std::endl;
 }
