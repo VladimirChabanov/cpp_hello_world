244245b9c7faaad739095b1a6e19c27cb7d12055
JunAlex <134843654+JunAlex@users.noreply.github.com>
2023-07-05 14:51:49 +0000

    Change date-test-1

--- tests/date-test.sh
+++ tests/date-test.sh
@@ -4,10 +4,9 @@ g++ app.cpp -o app
 
 output=$(./app)
 
-expected_output="05.07.2023"
-
-if [ "$output" == "$expected_output" ]; then
-echo "Вывод программы совпадает с ожидаемым: $output"
+if [ "$output" = "05.07.2023" ]
+then
+  echo "Вывод программы совпадает с ожидаемым: $output"
 else
-echo "Вывод программы не совпадает с ожидаемым. Ожидается: $expected_output, >
+  echo "Вывод программы не совпадает с ожидаемым. Ожидается: $expected_output, получено: $output"
 fi
