c69a3751f761d33ec6982af92428673b2e210cf1
Detrovv1 <135144707+Detrovv1@users.noreply.github.com>
2023-07-05 17:28:42 +0300

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -6,7 +6,7 @@ int main()
     auto now = std::chrono::system_clock::now();
     std::time_t end_time = std::chrono::system_clock::to_time_t(now);
  
-    std::cout << "Current Time and Date: " << std::ctime(&end_time) << std::endl;
+    std::cout << std::ctime(&end_time) << std::endl;
  
     return 0;
 }
