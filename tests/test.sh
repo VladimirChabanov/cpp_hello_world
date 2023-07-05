1ec4fe8029d892f7ebd660d8386c963e652dfedf
Detrovv1 <135144707+Detrovv1@users.noreply.github.com>
2023-07-05 17:43:58 +0300

    Create test.sh

---
+++ tests/test.sh
@@ -0,0 +1,13 @@
+#1/bin/bash
+
+output=$(../app)
+
+expected_output="05.07.2023"
+
+if [ "$output" == "$expected_output" ]; then
+        echo "Bepнo"
+        exit 0
+else
+        echo "Неверно"
+        exit 0
+fi
