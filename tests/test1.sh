26431ba79ea9b6036d969382044a4baa8d631536
Anastasia2804m <136562209+Anastasia2804m@users.noreply.github.com>
2023-07-05 13:53:44 +0300

    Update test1.sh

--- tests/test1.sh
+++ tests/test1.sh
@@ -1,24 +1,13 @@
-name: Pull Request To Dev
-run-name: Run Integration Tests
-on:
-pull_request:
-branches:
-- 'dev'
-jobs:
-integration_testing:
-runs-on: ubuntu-latest
-steps:
-- name: Checkout
-uses: actions/checkout@v3
-- name: Install requirements
-run: pip install -r requirements.txt
-- name: Run app
-run: python3 ./src/app/app.py &
-- name: Test GET-request
-run: |
-ANSW=$(curl http://127.0.0.1:5000/)
-if [ "$ANSW" != "05.07.2023" ]; then
-exit 1
+#!/bin/bash
+
+output=$(./app)
+
+expected_output="05.07.2023"
+
+if [ "$output" == "$expected_output" ]; then
+        echo "Successfull"
+        exit 0
 else
-echo "Integration Test PASS"
+        echo "Failed"
+        exit 1
 fi
