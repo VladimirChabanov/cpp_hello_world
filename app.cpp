d848931a0a8f4d5f998b38341c9487a88404abe3
Detrovv1 <135144707+Detrovv1@users.noreply.github.com>
2023-07-05 17:17:49 +0300

    Time_update

--- app.cpp
+++ app.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <chrono>
  
-int main(){
-    std::cout << "Hello World" << std::endl;
+int main()
+{
+    auto now = std::chrono::system_clock::now();
+    std::time_t end_time = std::chrono::system_clock::to_time_t(now);
+ 
+    std::cout << "Current Time and Date: " << std::ctime(&end_time) << std::endl;
+ 
+    return 0;
 }
