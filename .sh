6776bc791ec08ebebc62d20e51bc977820eaf981
Anastasia2804m <136562209+Anastasia2804m@users.noreply.github.com>
2023-07-05 13:50:59 +0300

    Update .sh

--- .sh
+++ .sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-g++ app.cpp -o app
-
 output=$(./app)
 
 expected_output="05.07.2023"
 
 if [ "$output" == "$expected_output" ]; then
+        echo "Successfull"
+        exit 0
+else
+        echo "Failed"
+        exit 1
+fi
