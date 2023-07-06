552e7a69c88b1419f4c7b8db25c72f000cf43a80
At1set <114298791+At1set@users.noreply.github.com>
2023-07-06 11:17:46 +0300

    Update unit_tests.sh
    Added test

--- unit_tests.sh
+++ unit_tests.sh
@@ -1,1 +1,13 @@
-echo "Unit Tests PASS"
+#!/bin/bash
+
+g++ app.cpp -o app
+
+output=$(./app)
+
+if [ "$output" = "06.07.2023" ]
+then
+echo "Вывод совпадает с текущей датой: $output"
+else
+echo "Вывод не совпадает с текущей датой. Текущая дата: $expected_output, вывод программы: $output"
+exit 1
+fi
